8588510f78cab55519acc66cbce94f6fd4c52e5e
alexxandrrar <alexandroovnaa@gmail.com>
2022-08-20 23:37:36 +0300

    feature/ edit-course-card

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -4,22 +4,23 @@ import s from "./CourseCard.module.css";
 
 const CourseCard = (props) => (
   <div className={s.courseCard}>
-    <strong className={s.name}>{props.course.title}</strong>
-    <div className={s.description}>{props.course.description}</div>
+    <div className={s.name}>
+      <strong>{props.course.title}</strong>
+      <div className={s.description}>{props.course.description}</div>
+    </div>
+
     <div className={s.info}>
       <div>
-        <strong>Authors:</strong>...
+        <strong>Authors:</strong> Somebody, Someone
       </div>
       <div>
-        <strong>Duration: </strong>
-        {props.course.duration}
+        <strong>Duration: </strong> {props.course.duration}
       </div>
       <div>
-        <strong>Created: </strong>
-        {props.course.creationDate}
+        <strong>Created: </strong> {props.course.creationDate}
       </div>
+      <Button className={s.btn} text="Show course" size="medium" />
     </div>
-    <Button className={s.btn} text="Show course" size="medium" />
   </div>
 );
 export default CourseCard;
